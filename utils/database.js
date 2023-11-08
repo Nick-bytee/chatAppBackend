3dc70c0f40c5e3793a348ee005d48facd073f478
nickbytee <nikhilkaushik2228@gmail.com>
2023-11-08 17:25:17 +0530

    test

--- utils/database.js
+++ utils/database.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-console.log('process.env.SQL_USERNAME, process.env.SQL_PASSWORD, process.env.SQL_HOST')
+console.log(process.env.SQL_USERNAME, process.env.SQL_PASSWORD, process.env.SQL_HOST)
 const sequelize = new sequelize_1.Sequelize('chatappdata', process.env.SQL_USERNAME, process.env.SQL_PASSWORD, { host: process.env.SQL_HOST, dialect: 'postgres' });
 exports.default = sequelize;
