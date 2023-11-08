fb97f8a2391164af61077309f067a43863c2c06c
nickbytee <nikhilkaushik2228@gmail.com>
2023-11-08 17:58:14 +0530

    test

--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-console.log(process.env.SQL_USERNAME, process.env.SQL_PASSWORD, process.env.SQL_HOST)
+console.log(process.env.SQL_USERNAME, process.env.SQL_PASSWORD, process.env.SQL_HOST, process.env.SQL_DBNAME)
 const sequelize = new sequelize_1.Sequelize(process.env.SQL_DBNAME, process.env.SQL_USERNAME, process.env.SQL_PASSWORD, { host: process.env.SQL_HOST, dialect: 'mysql' });
 exports.default = sequelize;
