7689dfc76bed81d02aff64e778720ae030f09c91
nickbytee <nikhilkaushik2228@gmail.com>
2023-11-08 17:43:17 +0530

    fixed db

--- utils/database.js
+++ utils/database.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 console.log(process.env.SQL_USERNAME, process.env.SQL_PASSWORD, process.env.SQL_HOST)
-const sequelize = new sequelize_1.Sequelize('chatappdata', process.env.SQL_USERNAME, process.env.SQL_PASSWORD, { host: process.env.SQL_HOST, dialect: 'postgres' });
+const sequelize = new sequelize_1.Sequelize(process.env.SQL_DBNAME, process.env.SQL_USERNAME, process.env.SQL_PASSWORD, { host: process.env.SQL_HOST, dialect: 'postgres' });
 exports.default = sequelize;
