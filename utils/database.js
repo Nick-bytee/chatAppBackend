18b48b6b7bb86d846d0c316629ab1276ec120672
nickbytee <nikhilkaushik2228@gmail.com>
2023-11-08 17:01:57 +0530

    fixed db connection

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('groupchat', 'root', 'Root123@#', { host: 'localhost', dialect: 'mysql' });
+const sequelize = new sequelize_1.Sequelize('chatappdata', proces.env.SQL_USERNAME, process.env.SQL_PASSWORD, { host: process.env.SQL_HOST, dialect: '' });
 exports.default = sequelize;
